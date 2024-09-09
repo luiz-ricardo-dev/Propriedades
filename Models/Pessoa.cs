e5fe6bf7a3ebe70acfc0ff5572f6e12546ba766b
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-09 12:50:32 -0400

    first commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Propriedades.Models
+{
+    public class Pessoa
+    {   //Criando as propridades
+        private string _nome;
+        private int _idade;
+        public string Nome 
+        { //Realizando validaçôes
+            get => _nome.ToUpper(); //Retornar todo em maiusculo
+            
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser Vazio");
+                }
+                _nome = value;
+            }
+        }
+        public int Idade
+         { 
+            get => _idade;
+            
+            set
+            {
+                if(value <= 0){
+                    throw new ArgumentException("A idade nao pode ser menor que zero");
+                }
+                _idade = value;
+            }
+            
+         }
+        //Criando um método
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Referenciando a classe 
+using Propriedades.Models;
+//Criando um objeto
+Pessoa p1 = new Pessoa();
+//Atribuindo valores
+p1.Nome = "Jose";
+p1.Idade = 14;
+//Chamando o método da classe
+p1.Apresentar();
