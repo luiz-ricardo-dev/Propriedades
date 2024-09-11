1fcb35d4132f79ef0e5cf9e97e757652a1ab2292
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-11 12:13:49 -0400

    Implementando alguns métodos

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -14,5 +14,24 @@ namespace Propriedades.Models
         {
             Alunos.Add(aluno);
         }
+
+        public int ObterQuantidadeDeAlunos()
+        {
+            return Alunos.Count;
+        }
+
+        public void RemoverAluno(Pessoa aluno)
+        {
+           Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos()
+        {
+            Console.WriteLine($"Alunos do curso de: {Nome}");
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto);    
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
 ﻿//Referenciando a classe 
 using Propriedades.Models;
-//Criando um objeto
+// //Criando um objeto
+// Pessoa p1 = new Pessoa();
+// //Atribuindo valores
+// p1.Nome = "Jose";
+// p1.Sobrenome = "Fulano";
+// p1.Idade = 14;
+// //Chamando o método da classe
+// p1.Apresentar();
+
+
 Pessoa p1 = new Pessoa();
-//Atribuindo valores
-p1.Nome = "Jose";
-p1.Sobrenome = "Fulano";
-p1.Idade = 14;
-//Chamando o método da classe
-p1.Apresentar();
+p1.Nome = "Thiago";
+p1.Sobrenome = "Lopes";
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Leonardo";
+p2.Sobrenome = "Algusto";
+
+Pessoa p3 = new Pessoa();
+p3.Nome = "Wesley";
+p3.Sobrenome = "Matheus";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.AdicionarAluno(p3);
+cursoDeIngles.ListarAlunos();
