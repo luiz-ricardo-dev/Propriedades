93a4ad371065c471f7569dcb07301f3499fc3053
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-17 12:26:40 -0400

    Alterada algumas opçoes de saida

--- obj/Debug/net8.0/Propriedades.AssemblyInfo.cs
+++ obj/Debug/net8.0/Propriedades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Propriedades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+226fd0f10ed208da85f2b61f10ec8b6609490d4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9aec83118bed5fd03a3290345447e11d3a78ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Propriedades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Propriedades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
